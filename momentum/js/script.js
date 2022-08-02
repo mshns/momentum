734a6ee1778a3e927a63db8a6101b84126c1e2fc
mshns <mishanos412@gmail.com>
2022-08-02 17:44:05 +0300

    feat: add greeting

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -9,5 +9,39 @@ function showTime() {
   const options = {month: 'long', day: 'numeric', weekday: 'long'};
   const currentDate = date.toLocaleDateString('en-En', options);
   showDate.textContent = currentDate;
+
+  function getTimeOfDay() {
+    const date = new Date();
+    const hours = date.getHours();
+    switch (Math.trunc(hours / 6)) {
+      case 3: return 'evening';
+        break;
+      case 2: return 'afternoon';
+        break;
+      case 1: return 'morning';
+        break;
+      case 0: return 'night';
+    }
+  }
+  const greeting = document.querySelector('.greeting');
+  const timeOfDay = getTimeOfDay();
+  greeting.textContent = `Good ${timeOfDay},`;
 }
 showTime();
+
+
+
+const yourName = document.querySelector('.name');
+
+
+function setLocalStorage() {
+  localStorage.setItem('name', yourName.value);
+}
+window.addEventListener('beforeunload', setLocalStorage)
+
+function getLocalStorage() {
+  if(localStorage.getItem('name')) {
+    yourName.value = localStorage.getItem('name');
+  }
+}
+window.addEventListener('load', getLocalStorage)
