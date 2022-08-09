d1cd1d00252ee64c1efc005d51f18623b627cad7
mshns <mishanos412@gmail.com>
2022-08-09 06:53:46 +0300

    fix: change music track paths

--- momentum/js/playList.js
+++ momentum/js/playList.js
@@ -1,22 +1,22 @@
 const playList = [
   {
     title: 'Sensorica − Aqua Caelestis',
-    src: '../assets/sounds/AquaCaelestis.mp3',
+    src: '../momentum/assets/sounds/Aqua Caelestis.mp3',
     duration: '00:39'
   },
   {
     title: 'Ennio Morricone − Chi Mai',
-    src: '../assets/sounds/EnnioMorricone.mp3',
+    src: '../momentum/assets/sounds/Ennio Morricone.mp3',
     duration: '01:37'
   },
   {
     title: 'Yiruma − River Flows In You',
-    src: '../assets/sounds/RiverFlowsInYou.mp3',
+    src: '../momentum/assets/sounds/River Flows In You.mp3',
     duration: '01:37'
   },
   {
     title: 'Summer Wind',
-    src: '../assets/sounds/SummerWind.mp3',
+    src: '../momentum/assets/sounds/Summer Wind.mp3',
     duration: '01:50'
   }
 ]
